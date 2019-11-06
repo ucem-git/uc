32902720895a0e5148c5da97af852506b029a288
Бойков Павел Олегович <boykov@em.mos.ru>
2019-11-06 16:41:03 +0300

    Создан парсер для hh.ru

---
+++ hh-parser.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+           'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+
+base_url = 'https://hh.ru/search/vacancy?L_is_autosearch=false&area=1&clusters=true&enable_snippets=true&text=python&page=0'
+
+def hh_parse(base_url, headers):
+    jobs = []
+    session = requests.Session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        for div in divs:
+            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+            text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+            text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+            content = text1 + ' ' + text2
+            jobs.append({
+                'title': title,
+                'href': href,
+                'company': company,
+                'content': content
+            })
+        print(len(jobs))
+    else:
+        print('ERROR')
+
+hh_parse(base_url, headers)
